b28e59bf9208e3dc2b76301ba06f1a6632652183
520Girl <815842080@qq.com>
2019-05-23 11:11:33 +0800

    project init

--- src/main.js
+++ src/main.js
@@ -1,8 +1,12 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import fastClick from 'fastclick' // 解决300毫秒click延迟
+import './assets/styles/reset.css' // 初始化各种标签,以达到每个机型显示
+import './assets/styles/border.css' // 一像素解决方案
 
 Vue.config.productionTip = false
+fastClick.attach(document.body); // 将这个方法绑定在网页上
 
 /* eslint-disable no-new */
 new Vue({
---
+++ src/pages/home/Home.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h1>这是h1标签</h1>
+    <h1>这是h1标签</h1>
+    <h1>这是h1标签</h1>
+    <a href="">this a标签</a>
+  </div>
+</template>
+<script>
+export default {
+  name: 'Home'
+}
+</script>
+<style lang="scss" scoped>
+</style>
